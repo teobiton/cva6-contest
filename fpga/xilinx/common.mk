b39ef2438e1f4bca992c8c251e7c079d14c6b49f
teobiton <teo.biton@gmail.com>
2022-12-09 12:22:30 +0100

    Change makefiles for install
    To complete the set up for the contest, these two files are
    modified, mainly to correctly detect the alias for Vitis and
    Vivado.
    
    Moreover, the Xilinx board spec is modified because it could not
    be found with a "0".

--- fpga/xilinx/common.mk
+++ fpga/xilinx/common.mk
@@ -1,11 +1,11 @@
 all:
-	vivado -mode batch -source tcl/run.tcl
+	$(VIVADO) -mode batch -source tcl/run.tcl
 	mkdir -p ip
 	cp -r ${PROJECT}.srcs/sources_1/ip/${PROJECT}/* ip/.
 	cp ${PROJECT}.runs/${PROJECT}_synth_1/${PROJECT}.dcp ip/.
 
 gui:
-	vivado -mode gui -source tcl/run.tcl &
+	$(VIVADO) -mode gui -source tcl/run.tcl &
 
 clean:
 	rm -rf ip/*
